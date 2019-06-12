9bce9ac8ca238dbdbcff7f1056ada3d73d0dc979
Clément <clement.petrault@utbm.fr>
2019-06-13 00:14:41 +0200

    feat: ajout tid homme-flux + file de message

--- atelier.h
+++ atelier.h
@@ -2,11 +2,15 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
 #include "custom_types.h"
 
 #define nbAtelier 10 //Nombre d'ateliers (nb de poste de conion)
 
 pthread_t tid[nbAtelier]; // Tableau des threads : un par atelier
+pthread_t homme_flux_tid; // Id thread homme-flux
 
 pthread_mutex_t mutex[nbAtelier]; // Tableau des mutexs pour chaque atelier
 pthread_mutex_t mutexStatus;
@@ -20,11 +24,13 @@ pthread_cond_t conditions[nbAtelier]; // Tableau de conditions : une par atelier
  */
 int statusAteliers[nbAtelier];
 
+int msgid; // Id de la file de message de l'homme-flux
+
 // Aire de collecte (où se trouve les conteneurs plein et vide)
 struct AireDeCollecte aireDeCollecte;
 
 struct CarteMagnetique *carteCourante;
 
-void *atelier_job(void* arg);
-void checkComposants(struct ParamAteliers* params);
-void envoiCarteMagnetique(struct Conteneur* arg);
+void *atelier_job(void *arg);
+void checkComposants(struct ParamAtelier *params);
+void envoiCarteMagnetique(struct Conteneur *arg);
